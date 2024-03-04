2c7c0fe8f9918cb96c516cba052981aff85381eb
Vishal Patel <vishal.patel@wolterskluwer.com>
2024-03-04 16:59:06 +0530

    Testing.

---
+++ UnitTestProject/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UnitTestProject
+{
+    internal class Calculator
+    {
+        // Implement the add function below here
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        // Implement the subtract function below here
+        public int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+
+        // Implement the multiply function below here
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        // Implement the divide function below here
+        public int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wolters Kluwer")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © Wolters Kluwer 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0f0e2f0d-94c3-4ded-ad5d-4a27e6f741c9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
